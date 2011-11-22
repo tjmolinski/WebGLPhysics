ce3ca0be15232501e674721e61e4f2f04b6a5641
TJ <tjmolinski@yahoo.com>
2011-11-22 16:44:35 -0600

    More plane tweaks

--- plane.js
+++ plane.js
@@ -37,20 +37,13 @@ Plane.prototype.checkForHits = function(elapsed, p)
 	
 	var dot = this.normal[0] * tmp[0] + this.normal[1]*tmp[1] + this.normal[2]*tmp[2];
 	
-	if(dot > 0.0)
-	{
-		if(this.penetration <= 0)
-			return;
-		
+	if(dot >= 0.0)
+	{		
 		if(this.particle.mass > 0.0)
 		{
-			this.particle.pos[0] += this.penetration * this.normal[0] * elapsed;
-			this.particle.pos[1] += this.penetration * this.normal[1] * elapsed;
-			this.particle.pos[2] += this.penetration * this.normal[2] * elapsed;
-			
-			this.particle.vel[0] = -this.particle.vel[0] * this.normal[0] * this.restitution;
-			this.particle.vel[1] = -this.particle.vel[1] * this.normal[1] * this.restitution;
-			this.particle.vel[2] = -this.particle.vel[2] * this.normal[2] * this.restitution;
+			this.particle.vel[0] = this.particle.vel[0] - (this.restitution+1) * (this.normal[0]*this.particle.vel[0]) * this.normal[0];
+			this.particle.vel[1] = this.particle.vel[1] - (this.restitution+1) * (this.normal[1]*this.particle.vel[1]) * this.normal[1]
+			this.particle.vel[2] = this.particle.vel[2] - (this.restitution+1) * (this.normal[2]*this.particle.vel[2]) * this.normal[2]
 		}
 	}
 }
