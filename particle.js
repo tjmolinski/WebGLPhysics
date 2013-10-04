93e0f0de6390aee3292afacd064c693eef3f17dc
TJ Molinski <tjmolinski@gmail.com>
2013-10-04 14:57:36 -0500

    Create particles on space bar

--- particle.js
+++ particle.js
@@ -9,7 +9,7 @@ function Particle(radius_t, mass_t, damp_t)
 	this.vertexNormalBuffer;
 	this.vertexTextureCoordBuffer;
 	this.vertexIndexBuffer;
-	
+
 	this.particleRotationMatrix = mat4.create();
 	mat4.identity(this.particleRotationMatrix);
 
@@ -19,8 +19,8 @@ function Particle(radius_t, mass_t, damp_t)
 	this.pos = [0.0, 0.0, 0.0];
 	this.vel = [0.0, 0.0, 0.0];
 	this.forceAccum = [0.0, 0.0, 0.0];
-	this.acc = [0.0, -9.8, 0.0];
-	
+	this.acc = [(Math.random()*10)-5, (Math.random()*10)-5, 0.0];
+
 	this.isSelected = 0;
 }
 
@@ -68,41 +68,41 @@ Particle.prototype.kick = function(x,y,z)
 {
 	if(this.mass <= 0.0)
 		return;
-		
+
 	this.forceAccum[0] += x;
 	this.forceAccum[1] += y;
 	this.forceAccum[2] += z;
 }
 
 Particle.prototype.animate = function(elapsed)
-{    	
+{
 	if(this.mass <= 0.0)
 		return;
 
 	if(Math.abs(this.vel[1]) < this.VELOCITYMIN)
-		this.vel[1] = 0;	
-		
+		this.vel[1] = 0;
+
 	this.pos[0] += this.vel[0] * elapsed;
 	this.pos[1] += this.vel[1] * elapsed;
 	this.pos[2] += this.vel[2] * elapsed;
-	
+
 	var tmpX = this.acc[0];
 	var tmpY = this.acc[1];
 	var tmpZ = this.acc[2];
 	tmpX += this.forceAccum[0] * 1.0/this.mass;
 	tmpY += this.forceAccum[1] * 1.0/this.mass;
 	tmpZ += this.forceAccum[2] * 1.0/this.mass;
-	
+
 	this.vel[0] += tmpX * elapsed;
 	this.vel[1] += tmpY * elapsed;
 	this.vel[2] += tmpZ * elapsed;
-		
+
 	this.vel[0] *= Math.pow(this.damping, elapsed);
 	this.vel[1] *= Math.pow(this.damping, elapsed);
-	this.vel[2] *= Math.pow(this.damping, elapsed);	
-		
+	this.vel[2] *= Math.pow(this.damping, elapsed);
+
 	this.forceAccum = [0.0, 0.0, 0.0];
-	
+
 	/*if(this.pos[1] < -3.5)
 	{
 		this.pos[1] = -3.5;
@@ -115,13 +115,13 @@ Particle.prototype.draw = function()
 	mat4.identity(mvMatrix);
 
 	mat4.translate(mvMatrix, [this.pos[0],this.pos[1],this.pos[2]]);
-	
+
 	mat4.multiply(mvMatrix, this.particleRotationMatrix);
 
 	gl.activeTexture(gl.TEXTURE0);
 	gl.bindTexture(gl.TEXTURE_2D, particleTexture[this.isSelected]);
 	gl.uniform1i(shaderProgram.samplerUniform, 0);
-	
+
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
